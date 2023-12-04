f48653afed1fbb5028bc02617b4b202b24790ed4
Monu <monuyadav10012000@gmail.com>
2023-12-04 10:43:39 +0530

    work commit

--- app/src/main/java/com/example/socialmedia/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/socialmedia/Fragments/HomeFragment.kt
@@ -3,8 +3,11 @@ package com.example.socialmedia.Fragments
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.example.socialmedia.R
 import com.example.socialmedia.databinding.FragmentHomeBinding
 
@@ -23,11 +26,16 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-
 
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+        setHasOptionsMenu(true)
+        (requireActivity() as AppCompatActivity).setSupportActionBar(binding.materialToolbar2)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.option_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
 
