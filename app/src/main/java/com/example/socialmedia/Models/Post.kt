085b3b6e61966d52d8fe03c6e15dbb1c0752fd0c
Monu <monuyadav10012000@gmail.com>
2023-12-05 18:14:01 +0530

    work commit

--- app/src/main/java/com/example/socialmedia/Models/Post.kt
+++ app/src/main/java/com/example/socialmedia/Models/Post.kt
@@ -4,11 +4,21 @@ class Post {
 
     var postUrl: String = ""
     var caption: String = ""
+    var name : String = ""
+    var time : String = ""
 
     constructor()
     constructor(postUrl: String, caption: String) {
         this.postUrl = postUrl
         this.caption = caption
     }
+
+    constructor(postUrl: String, caption: String, name: String, time: String) {
+        this.postUrl = postUrl
+        this.caption = caption
+        this.name = name
+        this.time = time
+    }
+
 
 }
