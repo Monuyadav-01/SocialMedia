d32e206b8241fa660ce9eb42c907864146194c7f
monu <monuyadav10012000@gmail.com>
2023-12-17 20:23:26 +0530

    init commit

--- app/src/main/java/com/example/socialmedia/Fragments/SearchFragment.kt
+++ app/src/main/java/com/example/socialmedia/Fragments/SearchFragment.kt
@@ -1,24 +1,88 @@
 package com.example.socialmedia.Fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.socialmedia.R
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.socialmedia.Models.User
+import com.example.socialmedia.Utils.USER_NODE
+import com.example.socialmedia.adapters.SearchAdapter
 import com.example.socialmedia.databinding.FragmentSearchBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.toObject
+import com.google.firebase.ktx.Firebase
 
 
 class SearchFragment : Fragment() {
 
-    private lateinit var binding: FragmentSearchBinding
+    lateinit var binding: FragmentSearchBinding
+
+    lateinit var adapter: SearchAdapter
+    private var userList = ArrayList<User>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-       binding = FragmentSearchBinding.inflate(inflater,container,false)
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        binding.searchUserNameRv.layoutManager = LinearLayoutManager(requireContext())
+        adapter = SearchAdapter(requireContext(), userList)
+        binding.searchUserNameRv.adapter = adapter
 
-        return binding.root
+//        Firebase.firestore.collection(USER_NODE).get().addOnSuccessListener {
+//
+//            var tempList = ArrayList<User>()
+//            userList.clear()
+//            if (it.isEmpty) {
+//
+//            } else {
+//                for (i in it.documents) {
+//                    if (i.id.toString().equals(Firebase.auth.currentUser!!.uid.toString())) {
+//                    } else {
+//                        val user: User = i.toObject<User>()!!
+//                        tempList.add(user)
+//                    }
+//
+//                }
+//            }
+//
+//
+//            userList.addAll(tempList)
+//            adapter.notifyDataSetChanged()
+//
+//        }
+
+        binding.searchUserButton.setOnClickListener {
+            val text = binding.searchUser.text.toString()
+            Firebase.firestore.collection(USER_NODE).whereEqualTo("name", text).get()
+                .addOnSuccessListener {
+                    val tempList = ArrayList<User>()
+                    userList.clear()
+
+                    if (it.isEmpty) {
+
+
+                    } else {
+                        for (i in it.documents) {
+
+                            if (i.id.toString() .equals(Firebase.auth.currentUser!!.uid.toString())) {
+
+                            } else {
+                                val user: User = i.toObject<User>()!!
+                                tempList.add(user)
+                            }
+                        }
+                        userList.addAll(tempList)
+                        adapter.notifyDataSetChanged()
+                    }
+                }
+        }
+
+            return binding.root
+        }
     }
-}
+
+
 
--- app/src/main/java/com/example/socialmedia/adapters/SearchAdapter.kt
+++ app/src/main/java/com/example/socialmedia/adapters/SearchAdapter.kt
@@ -1,4 +1,73 @@
 package com.example.socialmedia.adapters
 
-class SearchAdapter {
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.socialmedia.Models.User
+import com.example.socialmedia.R
+import com.example.socialmedia.Utils.FOLLOW
+import com.example.socialmedia.databinding.SearchViewBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class SearchAdapter(val context: Context, val userList: ArrayList<User>) :
+    RecyclerView.Adapter<SearchAdapter.ViewHolder>() {
+
+
+    inner class ViewHolder(var binding: SearchViewBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = SearchViewBinding.inflate(LayoutInflater.from(context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        var isFollow = true
+        Glide.with(context).load(userList[position].image).placeholder(R.drawable.user)
+            .into(holder.binding.profileImage)
+
+        Firebase.firestore.collection(Firebase.auth.currentUser?.email!! + FOLLOW)
+            .whereEqualTo("email", userList[position].email).get().addOnSuccessListener {
+                if (it.documents.size ==0) {
+                    isFollow = false
+
+                } else {
+                    holder.binding.followBtn.text = "UnFollow"
+                    isFollow = true
+                }
+            }
+        holder.binding.followBtn.setOnClickListener {
+
+            if (isFollow) {
+                Firebase.firestore.collection(Firebase.auth.currentUser?.email!! + FOLLOW)
+                    .whereEqualTo("email", userList[position].email).get().addOnSuccessListener {
+
+                        Firebase.firestore.collection(Firebase.auth.currentUser?.email + FOLLOW)
+                            .document(
+                                it.documents[0].id
+                            ).delete()
+                    }
+                holder.binding.followBtn.text = "Follow"
+                isFollow = false
+
+            }
+            else{
+                Firebase.firestore.collection(Firebase.auth.currentUser?.email!! + FOLLOW).document()
+                    .set(userList[position])
+                holder.binding.followBtn.text = "UnFollow"
+                isFollow = true
+            }
+
+        }
+        holder.binding.userName.text = userList[position].name
+    }
 }
